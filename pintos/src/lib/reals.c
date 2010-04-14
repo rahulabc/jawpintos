08e0deb2d4596f1c65a25233776aff7fe753d220
joonyeengyo <joonyeengyo@bb621b8c-2427-5c5d-253e-68c456886d0a>
2010-04-14 04:59:04 +0000

    reals library GNU Style Formatting

--- pintos/src/lib/reals.c
+++ pintos/src/lib/reals.c
@@ -3,50 +3,83 @@
 #define FIXED_P 17
 #define FIXED_Q 14
 
-int int_to_real(int n) {
-  return n*(1<<FIXED_Q);
+int 
+int_to_real(int n) 
+{
+  return n * (1 << FIXED_Q);
 }
 
-int floor_real_to_int(int x) { // floor(x)
-  return x/(1<<FIXED_Q);
+/* floor(x) */
+int 
+floor_real_to_int(int x) 
+{ 
+  return x / (1 << FIXED_Q);
 }
 
-int round_real_to_int(int x) { // round(x)
-  if(x>=0) {
-    return (x+(1<<FIXED_Q)/2)/(1<<FIXED_Q);
-  }
-  return (x-(1<<FIXED_Q)/2)/(1<<FIXED_Q);
+/* round(x) */
+int 
+round_real_to_int(int x) 
+{  
+  if (x >= 0) 
+    {
+      return (x + (1 << FIXED_Q) / 2) / (1 << FIXED_Q);
+    }
+  return (x - (1 << FIXED_Q) / 2) / (1 << FIXED_Q);
 }
 
-int sum_reals(int x, int y) { // x + y
-  return x+y;
+/* x + y */
+int 
+sum_reals(int x, int y) 
+{ 
+  return x + y;
 }
 
-int diff_reals(int x, int y) { // x - y
-  return x-y;
+/* x - y */
+int 
+diff_reals(int x, int y) 
+{ 
+  return x - y;
 }
 
-int sum_real_int(int x, int n) { // x + n*f
-  return x+n*(1<<FIXED_Q);
+/* x + n * f */
+int 
+sum_real_int(int x, int n) 
+{ 
+  return x + n * (1 << FIXED_Q);
 }
 
-int diff_real_int(int x, int n) { // x - n*f
-  return x-n*(1<<FIXED_Q);
+/* x - n * f */
+int 
+diff_real_int(int x, int n) 
+{ 
+  return x - n * (1 << FIXED_Q);
 }
 
-int mult_reals(int x, int y) { // x*y/f
-  return ((int64_t)x)*y/(1<<FIXED_Q);
+/* x * y / f */
+int 
+mult_reals(int x, int y) 
+{ 
+  return ((int64_t) x) * y / (1 << FIXED_Q);
 }
 
-int mult_real_int(int x, int n) { // x*n
-  return x*n;
+/* x * n */
+int 
+mult_real_int(int x, int n) 
+{ 
+  return x * n;
 }
 
-int div_reals(int x, int y) { // x/y
-  return ((int64_t)x)*(1<<FIXED_Q)/y;
+/* x / y */
+int 
+div_reals(int x, int y) 
+{ 
+  return ((int64_t) x) * (1 << FIXED_Q) / y;
 }
 
-int div_real_int(int x, int n) { // x/n
-  return x/n;
+/* x / n */
+int 
+div_real_int(int x, int n) 
+{ 
+  return x / n;
 }
 
--- pintos/src/lib/reals.h
+++ pintos/src/lib/reals.h
@@ -1,16 +1,16 @@
 #ifndef __LIB_REALS_H__
 #define __LIB_REALS_H__
 
-int int_to_real(int n);
-int floor_real_to_int(int x); // floor(x)
-int round_real_to_int(int x); // round(x)
-int sum_reals(int x, int y); // x + y
-int diff_reals(int x, int y); // x - y
-int sum_real_int(int x, int n); // x + n*f
-int diff_real_int(int x, int n); // x - n*f
-int mult_reals(int x, int y); // x*y/f
-int mult_real_int(int x, int n); // x*n
-int div_reals(int x, int y); // x/y
-int div_real_int(int x, int n); // x/n
+int int_to_real(int n);  
+int floor_real_to_int(int x);      // floor(x)
+int round_real_to_int(int x);      // round(x)
+int sum_reals(int x, int y);       // x + y
+int diff_reals(int x, int y);      // x - y
+int sum_real_int(int x, int n);    // x + n*f
+int diff_real_int(int x, int n);   // x - n*f
+int mult_reals(int x, int y);      // x*y/f
+int mult_real_int(int x, int n);   // x*n
+int div_reals(int x, int y);       // x/y
+int div_real_int(int x, int n);    // x/n
 
 #endif // __LIB_REALS_H__
