969df0838921cee43883b46579ad0cf53b161967
joonyeengyo <joonyeengyo@bb621b8c-2427-5c5d-253e-68c456886d0a>
2010-04-27 05:04:08 +0000

    minor error in the previous check in

--- project2/src/userprog/process.c
+++ project2/src/userprog/process.c
@@ -56,7 +56,7 @@ process_execute (const char *file_name)
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (exec_name, PRI_DEFAULT, start_process, fn_copy);
 
-  free (fn_copy2);
+  free (exec_name);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
