f0a3c4a94f9d170cf51c4205558b5920f304a6a6
madhavan.anand <madhavan.anand@gmail.com>
2010-05-12 05:45:38 +0000

    moving valloc stuff into frame.c

--- project3/src/userprog/process.c
+++ project3/src/userprog/process.c
@@ -20,7 +20,7 @@
 #include "threads/vaddr.h"
 #include "threads/malloc.h"
 #include "threads/synch.h"
-#include "vm/page.h"
+#include "vm/frame.h"
 
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
