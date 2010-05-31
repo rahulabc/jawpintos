241e33f5bd8b590ca12bc7ab477b0797c95ee8d3
woohyunjin <woohyunjin@bb621b8c-2427-5c5d-253e-68c456886d0a>
2010-05-31 04:03:54 +0000

    indexing done

--- trunk/project4/src/filesys/free-map.h
+++ trunk/project4/src/filesys/free-map.h
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include "devices/block.h"
+#include "filesys/inode.h"
 
 void free_map_init (void);
 void free_map_read (void);
@@ -11,7 +12,9 @@ void free_map_create (void);
 void free_map_open (void);
 void free_map_close (void);
 
-bool free_map_allocate (size_t, block_sector_t *);
+bool free_map_allocate_one (block_sector_t *);
+bool free_map_allocate_multiple (int, block_sector_t, 
+				 struct inode_disk *);
 void free_map_release (block_sector_t, size_t);
 
 #endif /* filesys/free-map.h */
